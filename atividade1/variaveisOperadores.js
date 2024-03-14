d61df74ae5fca1f8d4a124f957d4318bad93b7e0
BEATRIZ ROCHA DE ARAÚJO <beatriz.araujo@a.ucb.br>
2024-03-14 09:59:41 -0300

    resolução primeira atividade

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+const a = 6;
+const b = 3;
+
+let c = a + b;
+console.log(`O resultado da seguinte operação: ${a} + ${b} é igual a ${c}`);
+
+c = a - b;
+console.log(`O resultado da seguinte operação: ${a} - ${b} é igual a ${c}`);
+
+c = a * b;
+console.log(`O resultado da seguinte operação: ${a} * ${b} é igual a ${c}`);
+
+c = a / b;
+console.log(`O resultado da seguinte operação: ${a} / ${b} é igual a ${c}`);
+
