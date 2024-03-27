563d6a53e4562531049a62c004f82c8f1a7541a7
rochabea <biarochaaraujo@gmail.com>
2024-03-27 15:18:33 -0300

    atividade 3

---
+++ atividade3/matrizTransposta.js
@@ -0,0 +1,25 @@
+function transporMatriz(A){
+    let matrizString = '';
+    for(let i = 0; i < A.lenght; i++){
+        for(let x = 0; x < A[i].lenght; x++){
+            matrizString += A[i][x] + ' ';
+        }
+        matrizString += '\n';
+    }
+    console.log(matrizString);
+
+    let matrizTranspostaString = '';
+    for(let x = 0; x < A[0].lenght; x++){
+        for(let i = 0; i < A.lenght; i++){
+            matrizTranspostaString += A[i][x] + ' ';
+        }
+        matrizTranspostaString += '\n'
+    }
+    console.log(matrizTranspostaString)
+}
+
+const A = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
