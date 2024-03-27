8a68505b4794b76e3d70b66cd139412933c0c78c
rochabea <biarochaaraujo@gmail.com>
2024-03-27 15:27:18 -0300

    atividade 3

--- atividade3/matrizTransposta.js
+++ atividade3/matrizTransposta.js
@@ -1,27 +1,28 @@
 function transporMatriz(A){
     let matrizString = '';
-    for(let i = 0; i < A.lenght; i++){
-        for(let x = 0; x < A[i].lenght; x++){
-            matrizString += A[i][x] + ' ';
+    for(let i = 0; i < A.length; i++){
+        for(let j = 0; j < A[i].length; j++){
+            matrizString += A[i][j] + ' ';
         }
         matrizString += '\n';
     }
     console.log(matrizString);
 
     let matrizTranspostaString = '';
-    for(let x = 0; x < A[0].lenght; x++){
-        for(let i = 0; i < A.lenght; i++){
-            matrizTranspostaString += A[i][x] + ' ';
+    for(let j = 0; j < A[0].length; j++){
+        for(let i = 0; i < A.length; i++){
+            matrizTranspostaString += A[i][j] + ' ';
         }
-        matrizTranspostaString += '\n'
+        matrizTranspostaString += '\n';
     }
-    console.log(matrizTranspostaString)
+    console.log(matrizTranspostaString);
+
 }
 
 const A = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-];
+            [1, 2], 
+            [3, 4], 
+            [5, 6]
+        ];
 
 transporMatriz(A);
